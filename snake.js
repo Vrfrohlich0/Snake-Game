8e6e2f24f528bdc3477843cc31323f32e4f747f5
Vrfrohlich0 <129698709+Vrfrohlich0@users.noreply.github.com>
2023-04-08 19:53:22 -0300

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,92 @@
+// Set up the canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var cellSize = 10;
+var canvasWidth = canvas.width / cellSize;
+var canvasHeight = canvas.height / cellSize;
+
+// Set up the snake
+var snake = [];
+snake[0] = {
+    x: Math.floor(canvasWidth / 2),
+    y: Math.floor(canvasHeight / 2)
+};
+var dx = 1;
+var dy = 0;
+
+// Set up the food
+var food = {
+    x: Math.floor(Math.random() * canvasWidth),
+    y: Math.floor(Math.random() * canvasHeight)
+};
+
+// Set up the score and highscore
+var score = 0;
+var highscore = localStorage.getItem("highscore") || 0;
+document.getElementById("highscore").textContent = highscore;
+
+// Handle keyboard input
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 37 && dx !== 1) { // left arrow
+        dx = -1;
+        dy = 0;
+    } else if (event.keyCode === 38 && dy !== 1) { // up arrow
+        dx = 0;
+        dy = -1;
+    } else if (event.keyCode === 39 && dx !== -1) { // right arrow
+        dx = 1;
+        dy = 0;
+    } else if (event.keyCode === 40 && dy !== -1) { // down arrow
+        dx = 0;
+        dy = 1;
+    }
+});
+
+// Move the snake and check for collisions
+function moveSnake() {
+    var head = {
+        x: snake[0].x + dx,
+        y: snake[0].y + dy
+    };
+    snake.unshift(head);
+    if (head.x === food.x && head.y === food.y) {
+        food.x = Math.floor(Math.random() * canvasWidth);
+        food.y = Math.floor(Math.random() * canvasHeight);
+        score++;
+        document.getElementById("score").textContent = score;
+        if (score > highscore) {
+            highscore = score;
+            localStorage.setItem("highscore", highscore);
+            document.getElementById("highscore").textContent = highscore;
+        }
+    } else {
+        snake.pop();
+    }
+    if (head.x < 0 || head.x >= canvasWidth || head.y < 0 || head.y >= canvasHeight) {
+        clearInterval(gameInterval);
+        alert("Game over!");
+    }
+    for (var i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+            clearInterval(gameInterval);
+            alert("Game over!");
+        }
+    }
+}
+
+// Draw the snake and the food
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#333";
+    for (var i = 0; i < snake.length; i++) {
+        ctx.fillRect(snake[i].x * cellSize, snake[i].y * cellSize, cellSize, cellSize);
+    }
+    ctx.fillStyle = "#f00";
+    ctx.fillRect(food.x * cellSize, food.y * cellSize, cellSize, cellSize);
+}
+
+// Start the game loop
+var gameInterval = setInterval(function() {
+    moveSnake();
+    draw();
+}, 100);
